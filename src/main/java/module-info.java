77cb4fa391c8ed720a9e9960a087f31c023a7533
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:59:21 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/*
+ * ao-io-buffer - Output buffering library.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-io-buffer.
+ *
+ * ao-io-buffer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-io-buffer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-io-buffer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.io.buffer {
+	exports com.aoapps.io.buffer;
+	// Direct
+	requires com.aoapps.lang; // <groupId>com.aoapps</groupId><artifactId>ao-lang</artifactId>
+	requires com.aoapps.tempfiles; // <groupId>com.aoapps</groupId><artifactId>ao-tempfiles</artifactId>
+	requires org.apache.commons.lang3; // <groupId>org.apache.commons</groupId><artifactId>commons-lang3</artifactId>
+	// Java SE
+	requires java.logging;
+}
