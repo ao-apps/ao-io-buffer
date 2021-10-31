1a43aa4a7e1c771d4e7ad2789f02963733d7d49b
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:54:42 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-io-buffer.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-io-buffer.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.io.buffer {
 	exports com.aoapps.io.buffer;
