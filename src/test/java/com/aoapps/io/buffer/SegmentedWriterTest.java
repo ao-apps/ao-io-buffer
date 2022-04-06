63cf8615c8580a822125c0a64a56c4bd3fe4e927
AO Industries, Inc <support@aoindustries.com>
2022-04-06 16:07:52 -0500

    New nested MediaWriter for easy streaming encoding

--- src/test/java/com/aoapps/io/buffer/SegmentedWriterTest.java
+++ src/test/java/com/aoapps/io/buffer/SegmentedWriterTest.java
@@ -1,6 +1,6 @@
 /*
  * ao-io-buffer - Output buffering library.
- * Copyright (C) 2013, 2015, 2016, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2013, 2015, 2016, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -25,7 +25,7 @@ package com.aoapps.io.buffer;
 import com.aoapps.encoding.EncodingContext;
 import static com.aoapps.encoding.JavaScriptInXhtmlAttributeEncoder.javaScriptInXhtmlAttributeEncoder;
 import static com.aoapps.encoding.JavaScriptInXhtmlEncoder.javaScriptInXhtmlEncoder;
-import com.aoapps.encoding.MediaWriter;
+import com.aoapps.encoding.JavaScriptWriter;
 import static com.aoapps.encoding.TextInXhtmlAttributeEncoder.textInXhtmlAttributeEncoder;
 import com.aoapps.lang.io.NullWriter;
 import java.io.IOException;
@@ -4660,7 +4660,7 @@ public class SegmentedWriterTest extends BufferWriterTestParent {
 		writer[201].flush();
 		writer[201].close();
 		result[269] = writer[201].getResult();
-		result[269].writeTo(new MediaWriter(EncodingContext.DEFAULT, javaScriptInXhtmlEncoder, bodyContent));
+		result[269].writeTo(new JavaScriptWriter(EncodingContext.DEFAULT, javaScriptInXhtmlEncoder, bodyContent));
 		writer[202] = factory.newBufferWriter();
 		writer[202].close();
 		result[270] = writer[202].getResult();
@@ -5105,7 +5105,7 @@ public class SegmentedWriterTest extends BufferWriterTestParent {
 		writer[249].flush();
 		writer[249].close();
 		result[340] = writer[249].getResult();
-		result[340].writeTo(new MediaWriter(EncodingContext.DEFAULT, javaScriptInXhtmlEncoder, bodyContent));
+		result[340].writeTo(new JavaScriptWriter(EncodingContext.DEFAULT, javaScriptInXhtmlEncoder, bodyContent));
 		result[271].toString();
 		result[273].toString();
 	}
